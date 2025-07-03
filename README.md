85a5b0af04d38479747f9beb41ef59a62fe7177a
stedfast <fernando_unsc@hotmail.com>
2025-07-03 10:23:44 -0600

    Se crea la aplicacion web para analizar datos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RyR
 Repositorio para proyecto 7 RyR
+Este es un pequeño proyecto de prueba en el que se crea una aplicación web para analizar distintos datos informativos
+básicos sobre tipos de vehiculos en estados unidos.
---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import plotly.express
+
+car_data = pd.read_csv(r'C:\Users\Ferna\OneDrive\Desktop\SPRINT 7\RyR\vehicles_us.csv')
+
+st.header('DataFrame de Vehiculos')
+
+hist_button = st.button('Histograma')
+
+if hist_button:
+    st.write('Creacion de histograma para el conjunto de datos de anuncios para vehiculos')
+    #crear histograma
+    fig = px.histogram(car_data, x='odometer')
+    #mostrar el grafico
+    st.plotly_chart(fig, use_container_width=True)
+
+
+scatter_button = st.button('grafico de Dispersion')
+
+if scatter_button:
+    st.write('Creacion de grafico de dispersion del conjunto de datos vehiculares')
+    #se crea el grafico
+    fig = px.scatter(car_data, x='odometer')
+    #despues se muestra
+    st.plotly_chart(fig, use_container_width=True)
